bcc24355f8a70c145c6e5b45b3ba868b948638c7
iki1uc <entertrainer@web.de>
2026-09-06 06:00:26 +0200

    Create coord.js

---
+++ coord.js
@@ -0,0 +1,13 @@
+window.COORD = {
+  axes: {
+    b: "Breite",
+    h: "Höhe",
+    t: "Tiefe"
+  },
+  stages: {
+    "etage-8-orbit-viewer": { x: 3, y: 8, z: 1 },
+    "stage": { x: 1, y: 0, z: 0 },
+    "ui": { x: 2, y: 0, z: 0 },
+    "NC.suite": { x: 4, y: 4, z: 4 }
+  }
+};
